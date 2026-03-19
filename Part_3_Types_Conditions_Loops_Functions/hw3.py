5c63f8959533d6cd76b2c50c523ef488da2581cf
Alina Ilina <ilina.ar@phystech.edu>
2026-03-19 17:18:09 +0300

    done

--- Part_3_Types_Conditions_Loops_Functions/hw3.py
+++ Part_3_Types_Conditions_Loops_Functions/hw3.py
@@ -35,8 +35,10 @@ CostStats = tuple[float, float, dict[str, float]]
 def is_leap_year(year: int) -> bool:
     if year % 400 == 0:
         return True
+
     if year % 100 == 0:
         return False
+
     return year % 4 == 0
 
 
